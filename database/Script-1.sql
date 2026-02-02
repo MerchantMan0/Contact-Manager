b927927cc90fcd7aa6ad8f646a013d8f33a6700b
kylegiraldo <giraldokyle@gmail.com>
2026-02-02 14:45:41 -0500

    added database folder with script

---
+++ database/Script-1.sql
@@ -0,0 +1,20 @@
+USE small_project;
+
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	userFirstName VARCHAR(50),
+	userLastName VARCHAR(50),
+	username VARCHAR(50),
+	password VARCHAR(50),
+	creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+);
+
+CREATE TABLE contacts(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	firstName VARCHAR(50),
+	lastName VARCHAR(50),
+	email VARCHAR(50),
+	userID INT,
+	FOREIGN KEY (userID) REFERENCES users(id)
+);
+
